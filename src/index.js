c3ae683ab9382a6381e1983b20db4a21486bd0c1
Marina2901 <marm13@mail.ru>
2021-03-05 14:11:18 +0200

    docs: Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const map = new Map(bracketsConfig);
+  
+  const stack = [];
+  
+  
+  for (let char of str) {
+  
+    
+  if (char === map.get(stack[stack.length - 1])) stack.pop();
+  
+  else stack.push(char);
+  
+  }
+      
+  return !stack.length;
+    };
+  
